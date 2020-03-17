1504d361d28da3842146d690e81e7e4aa02f7d5e
whjuan <50222306+whjuan@users.noreply.github.com>
2020-03-17 16:29:58 +0800

    add

--- UVA11005.java
+++ UVA11005.java
@@ -6,47 +6,40 @@ public class UVA11005 {
 		Scanner sc = new Scanner(System.in);
   		
   		int t = sc.nextInt();
-  		for(int kase=1;kase<=t;kase++){
+  		for(int kase = 1;kase <= t;kase++){
   		
-  			int num[] = new int[10];
-  			int letter[] = new int[26];
-  			for(int i=0;i<10;i++)
-  				num[i] = sc.nextInt();
-  			for(int i=0;i<26;i++)
-  				letter[i] = sc.nextInt();
+  			int cost[] = new int[36];
+  			for(int i=0;i<36;i++) {
+  				cost[i] = sc.nextInt();
+  			}
   				
-  			System.out.println("Case "+kase+":");
+  			System.out.println("Case " + kase + ":");
   			
   			int q = sc.nextInt();
-  			while(q-->0){
-  				int n = sc.nextInt();
-  				int[] cost = new int[37];
+  			while(q-- > 0){
+  				int y = sc.nextInt();
+  				int sum[] = new int[37];
   				int min = Integer.MAX_VALUE;
-  				for(int base=2;base<=36;base++){
-  					String b = Integer.toString(n, base);
-  					int cnt = 0;
-  					for(int i=0;i<b.length();i++){
-  						char s = b.charAt(i);
-  						if('0' <= s && s <= '9'){
-  							cnt+=num[s-'0'];
-  						} 
-  						else{
-  							cnt+=letter[s-'a'];
-  						}
+  				for(int base = 2 ; base <= 36 ; base++){
+  					int n = y;
+  					while(n>0) {
+  						int r = n % base;
+  						sum[base] = sum[base] + cost[r];
+  						n = n / base;
   					}
-  					cost[base] = cnt;
-  					if(cnt<min)
-  						min = cnt;
+  					
+  					if(sum[base] < min)
+  						min = sum[base];
   				}
   				
-  				System.out.print("Cheapest base(s) for number "+n+":");
-  				for(int i=2;i<=36;i++){
-  					if(cost[i]==min)
-  						System.out.print(" "+i);
+  				System.out.print("Cheapest base(s) for number " + y + ":");
+  				for(int i = 2;i <= 36;i++){
+  					if(sum[i] == min)
+  						System.out.print(" " + i);
   				}
   				System.out.println();
   			}
-  			if(kase!=t)
+  			if(kase != t)
   				System.out.println();
   		}
 	}
