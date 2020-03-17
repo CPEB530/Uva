67072661391bcc0b60e3bc1e53b794b296971c78
asdzxant <58451139+asdzxant@users.noreply.github.com>
2020-03-17 18:46:34 +0800

    Update UVA10473.java

--- UVA10473.java
+++ UVA10473.java
@@ -5,29 +5,19 @@ public class UVA10473 {
 		Scanner sc=new Scanner(System.in);
 		while(true) {
 			String s=sc.next();
-			
-			
-			if(s.charAt(0)=='0') {
+
+			if(s.charAt(0)=='0'&&s.length()>=3) {
 				s=s.substring(2,s.length());
-				//System.out.println(s);
 				int val=Integer.valueOf(s,16);
 				System.out.println(val);
 				
 			}else {
-				if(Integer.parseInt(s)<0) {
+				int Num=Integer.parseInt(s);
+				if(Num<0) {
 					break;
 				}
-				String str=Integer.toHexString(Integer.parseInt(s));
-				
-				String ans="";
-				for(int i=0;i<str.length();i++) {
-					if(Character.isLetter(str.charAt(i))) {
-						ans+=Character.toUpperCase(str.charAt(i));
-					}else {
-						ans+=str.charAt(i);
-					}
-				}
-				System.out.println("0x"+ans);
+				String str=Integer.toHexString(Num).toUpperCase();
+				System.out.println("0x"+str);
 				
 				
 			}
