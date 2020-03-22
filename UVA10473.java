899f3304438b062ad9399c0bb77d6c3883e37917
asdzxant <58451139+asdzxant@users.noreply.github.com>
2020-03-22 13:06:59 +0800

    Update UVA10473.java

--- UVA10473.java
+++ UVA10473.java
@@ -8,21 +8,53 @@ public class UVA10473 {
 
 			if(s.charAt(0)=='0'&&s.length()>=3) {
 				s=s.substring(2,s.length());
-				int val=Integer.valueOf(s,16);
+				int val=toDec(s);
 				System.out.println(val);
-				
 			}else {
-				int Num=Integer.parseInt(s);
-				if(Num<0) {
+				if(s.charAt(0)=='-') {
 					break;
 				}
-				String str=Integer.toHexString(Num).toUpperCase();
-				System.out.println("0x"+str);
-				
-				
+				String str=toHex(s);
+				System.out.println(str);
 			}
 		}
 		
 	}
+	public static String toHex(String s) {
+		String ans="";
+		int n=Integer.parseInt(s);
+		int q=0;
+		if(n>0) {
+			while(n>0) {
+				q=n%16;
+				if(q<10) {
+					ans=Integer.toString(q)+ans;
+				}else {	
+					char a=(char)('A'+q-10);
+					ans=a+ans;	
+				}
+				n/=16;
+			}
+		}else {
+			ans="0";
+		}
+		
+		ans="0x"+ans;
+		return ans;
+	}
+	public  static int toDec(String s) {
+		int ans=0;
+		for(int i=s.length()-1,j=0;i>=0;i--,j++) {
+			char c=s.charAt(i);
+			if(c>='A'&&c<='F') {
+				int a=c-'A'+10;
+				ans+=a*Math.pow(16,j);
+			}else {
+				ans+=(c-'0')*Math.pow(16,j);
+			}
+		}
+		return ans;
+	}
+	
 
 }
