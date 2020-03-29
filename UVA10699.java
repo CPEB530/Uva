4d7d4cf10b996c4577f33616791c827c1e0a67f1
asdzxant <58451139+asdzxant@users.noreply.github.com>
2020-03-29 16:24:01 +0800

    Update UVA10699.java

--- UVA10699.java
+++ UVA10699.java
@@ -10,21 +10,19 @@ public class UVA10699 {
 			System.out.print(n+" : ");
 			int A[]=new int [n+1];
 			int i=2;
-			
+			int count=0;
 			while(n!=1) {
 				if(n%i==0) {
-					n/=i;
-					A[i]++;
+					count++;
+					while(n%i==0) {
+						n/=i;
+					}
 				}else {
 					i+=1;
 				}
 			}
-			int count=0;
-			for(int j=0;j<A.length;j++) {
-				if(A[j]!=0) {
-					count+=1;
-				}
-			}
+			
+			
 			System.out.println(count);
 			
 		}
